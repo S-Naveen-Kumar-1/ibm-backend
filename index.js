d0cb76330a040ba919f95818737823d7f6528bd4
S NAVEEN KUMAR <navee1110@gmail.com>
2024-01-26 20:28:54 +0530

    updated cors

--- index.js
+++ index.js
@@ -9,11 +9,7 @@ const server = http.createServer(app)
 const { userRouter } = require("./routes/user.routes")
 const { bugRouter } = require("./routes/bug.routes")
 app.use(express.json())
-const io = new Server(server, {
-    cors: {
-        origin: "http://localhost:3001/",
-    }
-})
+const io = new Server(server)
 io.on("connection", (socket) => {
     console.log(`User Connected ${socket.id}`)
     socket.on("join_room", (data) => {
