1a03329f41bd09e617abfd9e58aeb9e87c8e2c85
S NAVEEN KUMAR <navee1110@gmail.com>
2024-01-26 16:50:33 +0530

    backend ready to deploy

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const { bugRouter } = require("./routes/bug.routes")
 app.use(express.json())
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3001",
+        origin: "http://localhost:3000",
         methods: ["GET", "POST"]
     }
 })
